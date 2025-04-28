06c1ed805a895a355f51fb46fbd1f7e037140fef
smallest-cock <soccerstarslegend420@gmail.com>
2025-04-28 15:47:57 -0700

    fix submodule initialization script

--- init-submodules.bat
+++ init-submodules.bat
@@ -1,13 +1,23 @@
 @echo off
+git submodule init
 
-:: nlohmann/json (Shallow clone - saves ~195MB)
-git submodule update --init --depth=1 external/json
+:: Shallow clone for nlohmann/json (saves ~195MB)
+git submodule update --depth=1 external/json
 
-:: Branch-tracked submodules
-git submodule update --init --remote --branch main external/RLSDK
-git submodule update --init --remote --branch main external/ModUtils
-git submodule update --init --remote --branch master external/BMSDK
-git submodule update --init --remote --branch master external/BakkesmodPluginTemplate
+:: Configure branch-tracked submodules
+call :setup_branch_submodule external/RLSDK main
+call :setup_branch_submodule external/ModUtils main
+call :setup_branch_submodule external/BMSDK master
+call :setup_branch_submodule external/BakkesmodPluginTemplate master
 
-:: Verify
 git submodule status
+exit /b
+
+:setup_branch_submodule
+git submodule set-branch --branch %2 %1
+git submodule update --remote %1
+cd %1
+git checkout %2
+git pull
+cd ../..
+exit /b
