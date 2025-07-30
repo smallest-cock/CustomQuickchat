bb8b44f39cfbe00aa040852ad6525d090d5b597d
smallest-cock <soccerstarslegend420@gmail.com>
2025-07-29 19:33:23 -0700

    update version

--- CustomQuickchat/version.h
+++ CustomQuickchat/version.h
@@ -1,9 +1,8 @@
 #pragma once
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 9
-#define VERSION_PATCH 2
+#define VERSION_PATCH 3
 #define VERSION_BUILD 0
 
 #define stringify(a) stringify_(a)
 #define stringify_(a) #a
-
