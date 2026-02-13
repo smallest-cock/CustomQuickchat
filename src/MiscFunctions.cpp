d32c2e801d2e635ae6ff09897f0850d6f55fe33f
smallest-cock <48503773+smallest-cock@users.noreply.github.com>
2026-02-13 04:06:19 -0800

    apply func name change

--- src/MiscFunctions.cpp
+++ src/MiscFunctions.cpp
@@ -739,6 +739,6 @@ std::optional<std::string> CustomQuickchat::getCurrentRumbleItem() {
 #ifndef USE_SPEECH_TO_TEXT
 void CustomQuickchat::warnNoSTT() {
 	std::string message = "This version doesnt support speech-to-text. You can find that version on the github Releases page";
-	NotifyAndLog("Speech-To-Text", message, 5);
+	notifyAndLog("Speech-To-Text", message, 5);
 }
 #endif
